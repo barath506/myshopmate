7601931afb7e825baac235a854ac1bd0f5e999b3
barath506 <barath506@gmail.com>
2025-01-28 19:04:06 -0600

    first shopmate

---
+++ src/components/CartCard.js
@@ -0,0 +1,12 @@
+import "./CartCard.css";
+export const CartCard = ({ product }) => {
+  const { id, name, price, image } = product;
+  return (
+    <div className="cartCard">
+      <img src={image} alt={name} />
+      <p className="productName">{name}</p>
+      <p className="productPrice">${price}</p>
+      <button>Remove</button>
+    </div>
+  )
+}
